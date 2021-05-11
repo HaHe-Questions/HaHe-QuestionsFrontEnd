fd0984e6f746cdcbb864487b118a45c4fe457a1a
aleksiarpoo <aleksi.paunonen@myy.haaga-helia.fi>
2021-05-11 15:16:53 +0300

    V7 vaatimuksia

--- src/components/Kysely.js
+++ src/components/Kysely.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function Kysely(props) {
     const [kysymykset, setKysymykset] = useState([]);
     const params = useParams();
     const kyselyid = params.paramsid;
+    const [vastaukset, setVastaukset] = useState([]);
+
 
     useEffect(() => {
       console.log(kyselyid);
@@ -16,14 +21,33 @@ function Kysely(props) {
         .then(response => response.json())
         .then(data => setKysymykset(data))
         .catch(err => console.error(err))
-        }   
+        }
+
+    const inputChanged = (event) => {
+          setVastaukset({...vastaukset, [event.target.name]: event.target.value});
+          console.log(vastaukset);
+        }
+
+
 
       return(
           <div>
-              {kysymykset.map(kysymys => <div>{kysymys.kysymysteksti}</div>)}
+              {kysymykset.map(kysymys => <div>{kysymys.kysymysteksti}<br></br>
+              <TextField 
+                id="outlined-basic" 
+                label="Vastaus" 
+                variant="outlined" 
+                multiline
+                />
+                
+                </div>)}
+                <Button onClick={inputChanged} color="primary">
+                 Tallenna
+              </Button>
           </div>
       )   
     }
+  
 
 
 export default Kysely;
